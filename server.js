c77e3337eb5c0a0340c7c56052788fd749b44adf
nisaChampagne <nchampag23@gmail.com>
2021-03-23 12:18:38 -0400

    basic server

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var http = require('http'); // 1 - Import Node.js core module
 
-var server = http.createServer(function (req, res) {   // 2 - creating server
+var app = http.createServer(function (req, res) {   // 2 - creating server
 
     //handle incomming requests here..
 
 });
+var port = process.env.PORT || 5000
 
-server.listen(5000); //3 - listen for any incoming requests
+app.listen(port); //3 - listen for any incoming requests
 
-console.log('Node.js web server at port 5000 is running..')
+console.log("Node Server started on: " + port)
